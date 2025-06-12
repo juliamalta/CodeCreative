50ec97ee4ee4ca898ffebaa2f9f6f4bcc43a6e1c
juliamalta <juliamalta12@gmail.com>
2025-06-12 08:30:57 -0300

    fix:padding hero

--- src/components/sections/hero-section/hero-section.tsx
+++ src/components/sections/hero-section/hero-section.tsx
@@ -10,7 +10,7 @@ import { Button } from '@/components/ui/button'
 export function HeroSection({ title, text, desc, button1text, button2text }: HeroProps) {
     return (
         <>
-            <section className="mx-auto bg-color-haiti pt-16">
+            <section className="mx-auto bg-color-haiti pt-24">
                 <div className="flex flex-col items-center justify-center gap-8 text-center">
                     <div className="flex flex-col items-center justify-center text-center">
                         <h1 className="mb-6 text-4xl font-normal leading-tight tracking-tight text-white md:text-6xl">
@@ -19,7 +19,7 @@ export function HeroSection({ title, text, desc, button1text, button2text }: Her
                                 <span className="font-bold text-color-studio">{text}</span>
                             </AuroraText>
                         </h1>
-                        <p className="text-color-scampi w-full text-sm sm:text-base lg:w-2/5">{desc}</p>
+                        <p className="w-full text-sm text-color-scampi sm:text-base lg:w-2/5">{desc}</p>
                     </div>
                     <div className="flex w-1/2 justify-center gap-3">
                         <Button variant="herobutton" size="hero">
