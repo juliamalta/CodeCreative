b433ef9ad04b7ec86400990a8b91a9adb44eedcc
juliamalta <juliamalta12@gmail.com>
2025-06-23 21:01:14 -0300

    feat:border

--- src/components/sections/Navigations/NavigationHeader.tsx
+++ src/components/sections/Navigations/NavigationHeader.tsx
@@ -49,7 +49,7 @@ function NavigationHeader({ logo, navs, buttonLink }: NavigationHeaderProps) {
 
     return (
         <section className="sticky top-0 z-50 bg-color-haiti">
-            <nav className="container flex items-center justify-between py-6">
+            <nav className="container flex items-center justify-between border-b-2 border-color-persianBlue py-6">
                 <div className="flex items-start">
                     <Link className="block max-w-max" href="/">
                         <Image width={172} height={73} src={logo} alt="logo" />
