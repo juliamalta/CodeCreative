8d3175388bbe2a03984cddbeeebcc94266353459
Júlia Malta <juliamalta12@gmail.com>
2026-01-23 10:44:01 -0300

    fix:contact

--- src/components/sections/Contact/Contact.tsx
+++ src/components/sections/Contact/Contact.tsx
@@ -15,17 +15,18 @@ function Contact({ title, text, buttontext }: ContactProps) {
             </div>
             <div className="flex flex-col bg-color-haiti py-6 sm:flex-row">
                 <div className="mx-auto flex flex-col items-center justify-center gap-5 p-5 py-24 sm:p-8 lg:p-16">
-                    <div>
-                        <h1 className="text-center text-2xl font-semibold text-white sm:text-left sm:text-2xl lg:text-4xl">
-                            {title}
-                        </h1>
+                    <div className="flex flex-col items-center justify-center gap-3">
+                        <div>
+                            <h1 className="text-center text-2xl font-semibold text-white sm:text-left sm:text-2xl lg:text-4xl">
+                                {title}
+                            </h1>
+                        </div>
+                        <div className="w-full items-center">
+                            <p className="text-sm font-normal text-color-cblue sm:text-left sm:text-base lg:text-base">
+                                {text}
+                            </p>
+                        </div>
                     </div>
-                    <div className="2xl:max-w-2xl">
-                        <p className="text-sm font-normal text-color-cblue sm:text-left sm:text-base lg:text-base">
-                            {text}
-                        </p>
-                    </div>
-
                     <div className="flex flex-row items-center justify-center gap-4 sm:justify-normal">
                         <div className="hidden justify-end lg:flex">
                             <a
