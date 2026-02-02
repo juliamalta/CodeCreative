e9fbaa093d46942d069b0fd36eb502761dd658ef
Júlia Malta <juliamalta12@gmail.com>
2026-02-02 14:09:48 -0300

    fix:width hero

--- src/components/sections/hero-section/hero-section.tsx
+++ src/components/sections/hero-section/hero-section.tsx
@@ -15,7 +15,7 @@ export function HeroSection({ title, desc, button1text, button2text, titlePrimar
                 <div className="flex flex-col items-center justify-center gap-8 text-center">
                     <div className="flex flex-col items-center justify-center text-center">
                         <p className="text-base font-semibold text-color-scampi">{titlePrimary}</p>
-                        <h1 className="font-regular mb-6 w-2/3 text-4xl text-white md:text-6xl">{title}</h1>
+                        <h1 className="font-regular mb-6 w-full text-4xl text-white sm:w-2/3 md:text-6xl">{title}</h1>
                         <p className="sm:w-2/8 text-base text-color-cblue sm:text-base md:w-2/3 lg:w-2/5">{desc}</p>
                     </div>
                     <div className="flex w-1/2 justify-center gap-3">
