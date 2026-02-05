cd8c090c3aefe02e11e4fee690cb2127109dbb38
Júlia Malta <juliamalta12@gmail.com>
2026-02-05 11:40:50 -0300

    fix:next

--- next.config.js
+++ next.config.js
@@ -31,8 +31,6 @@ const securityHeaders = [
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-
     images: {
         unoptimized: true, // desabilita otimização de imagens
     },
