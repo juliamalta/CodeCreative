e74fda1f89383bea8a8fb40a2a4a1ad07e61ae53
Júlia Malta <juliamalta12@gmail.com>
2026-02-10 12:12:54 -0300

    fix:hero

--- src/components/sections/hero-section/hero-section.tsx
+++ src/components/sections/hero-section/hero-section.tsx
@@ -22,7 +22,9 @@ export function HeroSection({ title, desc, button1text, button2text, titlePrimar
                     <div className="flex flex-col gap-8">
                         <div className="flex flex-col gap-6">
                             <HyperText className="text-base font-semibold text-color-purble">{titlePrimary}</HyperText>
-                            <h1 className="font-regular w-full text-3xl text-white md:text-6xl lg:w-2/4">{title}</h1>
+                            <h1 className="font-regular w-full text-3xl text-white md:text-6xl lg:w-2/3 2xl:w-2/4">
+                                {title}
+                            </h1>
                             <p className="sm:w-2/8 text-base text-white sm:text-base md:w-2/3 lg:w-2/5">{desc}</p>
                         </div>
                         <div className="flex flex-col gap-3 sm:flex-row">
