fe37bee64e2231b188ee160a30e24976175cedbb
Júlia Malta <juliamalta12@gmail.com>
2026-02-09 22:58:15 -0300

    feat:hypertext

--- src/components/sections/hero-section/hero-section.tsx
+++ src/components/sections/hero-section/hero-section.tsx
@@ -9,6 +9,7 @@ import { AnimatedBackground } from '@/components/ui/animated-background'
 import { Button } from '@/components/ui/button'
 import { InteractiveHoverButton } from '@/components/magicui/Interactive-HoverButton'
 import { SpotlightBackground } from '@/components/ui/spotlight'
+import { HyperText } from '@/components/magicui/hyper'
 
 export function HeroSection({ title, desc, button1text, button2text, titlePrimary }: HeroProps) {
     return (
@@ -20,7 +21,7 @@ export function HeroSection({ title, desc, button1text, button2text, titlePrimar
                 <div className="container flex flex-col gap-8">
                     <div className="flex flex-col gap-8">
                         <div className="flex flex-col gap-6">
-                            <p className="text-base font-semibold text-white">{titlePrimary}</p>
+                            <HyperText className="text-base font-semibold text-color-purble">{titlePrimary}</HyperText>
                             <h1 className="font-regular w-full text-3xl text-white md:text-6xl lg:w-2/4">{title}</h1>
                             <p className="sm:w-2/8 text-base text-white sm:text-base md:w-2/3 lg:w-2/5">{desc}</p>
                         </div>
