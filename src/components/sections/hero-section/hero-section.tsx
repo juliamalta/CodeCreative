73518f5a812148d248c682cf2b90254669360995
Júlia Malta <juliamalta12@gmail.com>
2026-02-10 12:02:32 -0300

    fix:global.css

--- src/components/sections/hero-section/hero-section.tsx
+++ src/components/sections/hero-section/hero-section.tsx
@@ -16,7 +16,7 @@ export function HeroSection({ title, desc, button1text, button2text, titlePrimar
         <>
             <section
                 id="inicio"
-                className="mx-auto bg-cover bg-center px-2 pb-44 pt-8 sm:py-44"
+                className="mx-auto bg-cover bg-center px-2 py-16 sm:py-44"
                 style={{ backgroundImage: "url('/images/codebg.png')" }}>
                 <div className="container flex flex-col gap-8">
                     <div className="flex flex-col gap-8">
