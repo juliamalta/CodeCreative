41e3359251a7a591bab258828bf6216020f22e3a
Júlia Malta <juliamalta12@gmail.com>
2026-02-13 20:31:29 -0300

    fix:favicon

--- src/configs/metadata.config.ts
+++ src/configs/metadata.config.ts
@@ -19,15 +19,15 @@ export const metadataConfig = {
     },
     icons: {
         icon: [
-            { url: 'favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-            { url: 'favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-            { url: 'favicon/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
-            { url: 'favicon/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+            { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+            { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+            { url: '/favicon/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+            { url: '/favicon/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
         ],
-        shortcut: 'favicon/favicon.ico',
-        apple: 'favicon/apple-touch-icon.png',
+        shortcut: '/favicon/favicon.ico',
+        apple: '/favicon/apple-touch-icon.png',
     },
-    manifest: 'favicon/site.webmanifest',
+    manifest: '/favicon/site.webmanifest',
     robots: {
         index: true,
         follow: true,
