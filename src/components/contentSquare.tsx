cfe2499fc5b1e85878dfebfe7b5403f7c54e782d
Júlia Malta <juliamalta12@gmail.com>
2026-04-30 10:04:19 -0300

    fix:contentSquare

--- src/components/contentSquare.tsx
+++ src/components/contentSquare.tsx
@@ -1,13 +1,9 @@
 'use client'
 
-import Script from "next/script"
+import Script from 'next/script'
 
 export function Contentsquare() {
-  return (
-    <Script
-      id="contentsquare"
-      strategy="afterInteractive"
-      src="https://t.contentsquare.net/uxa/415e683ec2af4.js"
-    />
-  )
+    return (
+        <Script id="contentsquare" strategy="afterInteractive" src="https://t.contentsquare.net/uxa/415e683ec2af4.js" />
+    )
 }
